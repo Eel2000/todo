7173f31fab89a9e7262ebb5a25d5b2e5168eb5d7
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2024-03-13 17:13:01 +0200

    nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">Welcome to home</h1>
-    </main>
-  );
+    return (
+        <main className="grid">
+            <nav className="flex gap-3 bg-blue-200 shadow-2xl h-[60px] p-2">
+                <h1 className="text-3xl mx-4">Code24</h1>
+                <ul className="flex gap-4 mt-2">
+                    <Link href="/pages/todo/"
+                          className="hover:text-white">
+                        <li>
+                            Todos
+                        </li>
+                    </Link>
+                    <Link href="/pages/counter/" className="hover:text-white">
+                        <li>
+                            Counter
+                        </li>
+                    </Link>
+                    <Link href="/pages/weather/" className="hover:text-white">
+                        <li>
+                            weather forecast
+                        </li>
+                    </Link>
+                </ul>
+            </nav>
+        </main>
+    );
 }
---
+++ src/app/pages/weather/page.tsx
@@ -0,0 +1,7 @@
+export default function page(){
+    return(
+        <div>
+            <h1>Welcome to the weather forecast</h1>
+        </div>
+    )
+}
